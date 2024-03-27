48f60d07e6c84eed1f8a4effc0db80841987a7dc
121210481 ALEXANDRE SOUTO MEDEIROS <alexandre.souto.medeiros@lcc3-073.lcc.ufcg.edu.br>
2024-03-27 10:56:13 -0300

    #us1 - testes robustos

---
+++ test/Us1Test.java
@@ -0,0 +1,19 @@
+package test;
+import Biblioteca.Biblioteca;
+import Biblioteca.Livro;
+import junit.framework.*;
+
+class Us1Test extends TestCase {
+
+
+    public void testeGetLivrosByTitulo(){
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.criarLivro("livro1");
+        biblioteca.criarLivro("livro2");
+        biblioteca.criarLivro("livro3");
+
+        Livro livro = biblioteca.buscarLivros("livro2");
+
+        assertEquals(livro.getTitulo(), "livro2");
+    }
+}
